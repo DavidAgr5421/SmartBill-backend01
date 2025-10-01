44110046328b48aa4a57a250bf8d606b33ff3522
David Agudelo <david542agr@gmail.com>
2025-10-01 03:45:47 -0500

    Vercel url

--- src/main/java/sena/facturacion/infrastructure/adapters/security/config/SecurityConfig.java
+++ src/main/java/sena/facturacion/infrastructure/adapters/security/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://smartbill-frontend.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
